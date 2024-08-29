625b4f3a6d2abbd19a6856d98265e58e0889a7a2
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-08-29 19:14:13 +0000

    chore: internal macro to combine strings to lit

--- origami-macros/src/utils.rs
+++ origami-macros/src/utils.rs
@@ -16,3 +16,34 @@ macro_rules! bail {
     };
 }
 pub(crate) use bail;
+
+macro_rules! combine_to_lit {
+    ($($input:expr),*) => {{
+        combine_to_lit!(@internal span => Span::call_site(), $($input),*)
+    }};
+    ($span:expr => $($input:expr),*) => {{
+        combine_to_lit!(@internal span => $span, $($input),*)
+    }};
+    (@internal span => $span:expr, $($input:expr),*) => {{
+        let mut s = String::new();
+        $(s.push_str(&$input);)*
+        LitStr::new(&s, $span)
+    }}
+}
+pub(crate) use combine_to_lit;
+
+#[cfg(test)]
+mod tests {
+    use proc_macro2::Span;
+    use syn::{Ident, LitStr};
+
+    #[test]
+    fn combine_lit() {
+        let lit = LitStr::new("test", Span::call_site());
+        let ident = Ident::new("test", Span::call_site());
+        assert_eq!(
+            combine_to_lit!(lit.span() => "foo", "bar", lit.value(), ident.to_string()),
+            LitStr::new("foobartesttest", Span::call_site())
+        );
+    }
+}
