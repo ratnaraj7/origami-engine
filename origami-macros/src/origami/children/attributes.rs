6a3c4936556595ee6adc753e0f4088388e3aa28e
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-08-29 19:23:17 +0000

    refactor: use `IndexMap` for insertion order, optimize string concatenation, and add tests
    - Replaced `HashMap` with `IndexMap` to maintain insertion order for `Attributes`.
    - Combined multiple `push_str` calls into a single call when dealing with literals for better performance.
    - Added tests to verify the new behavior and ensure correctness.

--- origami-macros/src/origami/children/attributes.rs
+++ origami-macros/src/origami/children/attributes.rs
@@ -1,18 +1,17 @@
-use std::collections::HashMap;
-
+use indexmap::IndexMap;
+use proc_macro2::Span;
 use quote::{quote, quote_spanned, ToTokens};
 use syn::parse::{Parse, ParseStream};
 use syn::spanned::Spanned;
 use syn::token::{Brace, Paren};
-use syn::{parenthesized, parse_quote, Expr, ExprField, Ident, LitChar, LitStr, Token};
+use syn::{parenthesized, Expr, ExprField, Ident, LitStr, Token};
 
-use crate::utils::bail;
 use crate::utils::kw::{escape, noescape};
+use crate::utils::{bail, combine_to_lit};
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 pub enum AttributeKey {
     LitStr(LitStr),
-    LitChar(LitChar),
     Ident(Ident),
     Iter(ExprField),
     #[cfg(feature = "html_escape")]
@@ -45,9 +44,6 @@ impl Parse for AttributeKey {
         if input.peek(LitStr) {
             return Ok(Self::LitStr(input.parse()?));
         }
-        if input.peek(LitChar) {
-            return Ok(Self::LitChar(input.parse()?));
-        }
         if input.peek(Token![..]) {
             input.parse::<Token![..]>()?;
             if !input.peek(Token![self]) {
@@ -56,7 +52,6 @@ impl Parse for AttributeKey {
             let expr: ExprField = input.parse()?;
             return Ok(Self::Iter(expr));
         }
-
         bail!(input, "Expected an attribute key.")
     }
 }
@@ -64,7 +59,6 @@ impl Parse for AttributeKey {
 #[derive(Debug)]
 pub enum AttributeValue {
     LitStr(LitStr),
-    LitChar(LitChar),
     Expr(Expr),
 }
 
@@ -72,9 +66,6 @@ impl Parse for AttributeValue {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         if input.peek(LitStr) {
             return Ok(Self::LitStr(input.parse()?));
-        }
-        if input.peek(LitChar) {
-            return Ok(Self::LitChar(input.parse()?));
         }
         if input.peek(Paren) {
             let content;
@@ -86,17 +77,11 @@ impl Parse for AttributeValue {
 }
 
 #[derive(Debug)]
-pub struct Attributes(
-    pub HashMap<AttributeKey, Option<AttributeValue>>,
-    // for key ordering
-    Vec<AttributeKey>,
-);
+pub struct Attributes(pub IndexMap<AttributeKey, Option<AttributeValue>>);
 
 impl Parse for Attributes {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let mut map = HashMap::new();
-        let mut position_map = HashMap::new();
-        let mut count = 0;
+        let mut map = IndexMap::new();
         while !input.peek(Brace) && !input.peek(Token![;]) {
             let key = input.parse::<AttributeKey>()?;
             let value = match key {
@@ -113,91 +98,164 @@ impl Parse for Attributes {
                 }
             };
             map.insert(key.clone(), value);
-            position_map.insert(key, count);
-            count += 1;
         }
-        let mut positions: Vec<_> = position_map.into_iter().collect();
-        positions.sort_by(|a, b| a.1.cmp(&b.1));
-        let positions: Vec<_> = positions.into_iter().map(|(key, _)| key).collect();
-        Ok(Self(map, positions))
+        Ok(Self(map))
+    }
+}
+
+enum AttributeType {
+    Lit(LitStr),
+    TokenStream(proc_macro2::TokenStream),
+}
+
+impl ToTokens for AttributeType {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        match self {
+            Self::Lit(lit) => tokens.extend(quote! {s.push_str(#lit);}),
+            Self::TokenStream(ts) => ts.to_tokens(tokens),
+        }
     }
 }
 
 impl ToTokens for Attributes {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        for k in &self.1 {
-            let v = match self.0.get(k) {
-                Some(v) => v,
-                None => unreachable!(),
-            };
+        let mut attribute_types = Vec::new();
+        for (k, v) in &self.0 {
+            let last = attribute_types.last_mut();
             match k {
                 AttributeKey::Ident(ident) => {
-                    tokens.extend(quote! {
-                        s.push(' ');
-                    });
-                    let literal = ident.to_string();
-                    let literal: LitStr = parse_quote! {
-                        #literal
-                    };
-                    tokens.extend(quote! {
-                        s.push_str(#literal);
-                    });
+                    if let Some(AttributeType::Lit(lit)) = last {
+                        let new_lit =
+                            combine_to_lit!(ident.span() => lit.value(), " ", ident.to_string());
+                        *lit = new_lit;
+                    } else {
+                        let lit = combine_to_lit!(ident.span() => " ", ident.to_string());
+                        attribute_types.push(AttributeType::Lit(lit));
+                    }
                 }
                 AttributeKey::LitStr(literal) => {
-                    tokens.extend(quote! {
-                        s.push(' ');
-                    });
-                    tokens.extend(quote! {
-                        s.push_str(#literal);
-                    });
-                }
-                AttributeKey::LitChar(literal) => {
-                    tokens.extend(quote! {
-                        s.push(' ');
-                    });
-                    tokens.extend(quote! {
-                        s.push(#literal);
-                    });
-                }
-                AttributeKey::Iter(expr) => tokens.extend(quote_spanned! {
-                    expr.span() =>
-                    for (key, val) in #expr.iter() {
-                        s.push(' ');
-                        s.push_str(key);
-                        s.push_str("=\"");
-                        s.push_str(val);
-                        s.push('"');
+                    if let Some(AttributeType::Lit(lit)) = last {
+                        let new_lit =
+                            combine_to_lit!(literal.span() => lit.value(), " ", literal.value());
+                        *lit = new_lit;
+                    } else {
+                        let lit = combine_to_lit!(literal.span() => " ", literal.value());
+                        attribute_types.push(AttributeType::Lit(lit));
                     }
-                }),
+                }
+                AttributeKey::Iter(expr) => {
+                    attribute_types.push(AttributeType::TokenStream(quote_spanned! {
+                        expr.span() =>
+                        for (key, val) in #expr.iter() {
+                            s.push(' ');
+                            s.push_str(key);
+                            s.push_str("=\"");
+                            s.push_str(val);
+                            s.push('"');
+                        }
+                    }))
+                }
                 #[cfg(feature = "html_escape")]
                 AttributeKey::Escape | AttributeKey::NoEscape => {}
             }
+            let last = attribute_types.last_mut();
             if let Some(v) = v {
-                tokens.extend(quote! {
-                    s.push_str("=\"");
-                });
                 match v {
                     AttributeValue::LitStr(literal) => {
-                        tokens.extend(quote! {
-                            s.push_str(#literal);
-                        });
-                    }
-                    AttributeValue::LitChar(literal) => {
-                        tokens.extend(quote! {
-                            s.push(#literal);
-                        });
+                        if let Some(AttributeType::Lit(lit)) = last {
+                            let new_lit = combine_to_lit!(literal.span() => lit.value(), "=\"", literal.value(), "\"");
+                            *lit = new_lit;
+                        } else {
+                            let lit =
+                                combine_to_lit!(literal.span() => "=\"", literal.value(), "\"");
+                            attribute_types.push(AttributeType::Lit(lit));
+                        }
                     }
                     AttributeValue::Expr(expr) => {
-                        tokens.extend(quote_spanned! {
+                        if let Some(AttributeType::Lit(lit)) = last {
+                            let new_lit = combine_to_lit!(lit.value(), "=\"");
+                            *lit = new_lit;
+                        } else {
+                            attribute_types
+                                .push(AttributeType::Lit(LitStr::new("=\"", expr.span())));
+                        }
+                        attribute_types.push(AttributeType::TokenStream(quote_spanned! {
                             expr.span() =>
                             s.push_str(#expr);
-                        });
+                        }));
+                        attribute_types.push(AttributeType::TokenStream(quote! {
+                            s.push('"');
+                        }));
                     }
                 }
-                tokens.extend(quote! {
-                    s.push('"');
-                });
             }
         }
+        tokens.extend(quote! {
+            #(#attribute_types)*
+        });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use syn::parse_quote;
+
+    use super::*;
+
+    #[derive(Debug)]
+    struct TestAttributes(Attributes);
+
+    impl Parse for TestAttributes {
+        fn parse(input: ParseStream) -> syn::Result<Self> {
+            let attrs: Attributes = input.parse()?;
+            input.parse::<Token![;]>()?;
+            Ok(Self(attrs))
+        }
+    }
+
+    #[cfg(feature = "html_escape")]
+    #[test]
+    fn should_work() {
+        let TestAttributes(attrs) = parse_quote! {
+            class="test" escape style="test" id=(self.id) ..self.k noescape class="test2" bar;
+        };
+        let tokens = quote! {
+            #attrs
+        };
+        assert_eq!(
+            tokens.to_string(),
+            quote! {
+                s.push_str(" class=\"test2\" style=\"test\" id=\"");
+                s.push_str(self.id);
+                s.push('"');
+                for (key, val) in self.k.iter() {
+                    s.push(' ');
+                    s.push_str(key);
+                    s.push_str("=\"");
+                    s.push_str(val);
+                    s.push('"');
+                }
+                s.push_str(" bar");
+            }
+            .to_string()
+        );
+    }
+
+    #[cfg(not(feature = "html_escape"))]
+    #[test]
+    #[should_panic(expected = "Enable `html_escape` feature to use `\"escape\"`.")]
+    fn should_panic_if_escape_is_used_when_feature_is_disabled() {
+        let TestAttributes(attrs) = parse_quote! {
+            class="test" escape;
+        };
+    }
+
+    #[cfg(not(feature = "html_escape"))]
+    #[test]
+    #[should_panic(expected = "Enable `html_escape` feature to use `\"noescape\"`.")]
+    fn should_panic_if_noescape_is_used_when_feature_is_disabled() {
+        let TestAttributes(attrs) = parse_quote! {
+            class="test" noescape;
+        };
     }
 }
